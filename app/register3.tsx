f2bc633dd2cff3d863c6b6c1d085de98a80cbea7
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-10-02 20:15:57 -0300

    feat: logica de clicar no botao e selecionar img

--- app/register3.tsx
+++ app/register3.tsx
@@ -1,96 +1,120 @@
-import { Text, View, Image, TextInput, TouchableOpacity, KeyboardAvoidingView, ScrollView  } from 'react-native';
+import { Text, View, Image, TextInput, TouchableOpacity, KeyboardAvoidingView, ScrollView } from 'react-native';
 import { colors } from '../utils/colors';
 import { Link } from 'expo-router';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 const PlaceholderImage = require('../assets/logo/nexaspay.logo.png');
-const GrowBar = require('../assets/growbar3.png')
+const GrowBar = require('../assets/growbar3.png');
+
 export default function Register1() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
-    <KeyboardAvoidingView
-      style={{
-        flex: 1,
-        backgroundColor: colors.bgDark4,
-        paddingHorizontal: 20,
-      }}
-    >
-     <ScrollView contentContainerStyle={{   }}>
-      <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', paddingTop: 40 }}>
-        <Image
-          source={PlaceholderImage}
-          style={{ width: 150, height: 150 }}
-          resizeMode="contain"
-        />
-        <Text style={{ color: '#fff', fontWeight: 'bold',  fontSize: 40 , padding:30}}>Criar Conta</Text>
-      <Image source={GrowBar} style={{marginBottom:30}}/>
-      </View>
-
-      <View style={{ flex: 1, justifyContent: 'flex-start', width: '100%' }}>
-        <Text style={{ color: '#fff', marginBottom: 5, fontWeight: 'medium',  fontSize: 16 }}>Endereço</Text>
-        <TextInput
-          placeholder="Digite seu endereço"
-          placeholderTextColor="#dbdbdb"
-          style={{
-            backgroundColor: colors.bgDark1,
-            padding: 15,
-            borderRadius: 10,
-            color: '#fff',
-            marginBottom: 15,
-          }}
-        /><Text style={{ color: '#fff', marginBottom: 5, fontWeight: 'medium',  fontSize: 16 }}>Celular</Text>
-        <TextInput
-          placeholder="Digite seu celular"
-          placeholderTextColor="#dbdbdb"
-          style={{
-            backgroundColor: colors.bgDark1,
-            padding: 15,
-            borderRadius: 10,
-            color: '#fff',
-            marginBottom: 15,
-          }}
-        />
-  <Text style={{ color: '#fff', marginBottom: 5, fontWeight: 'medium',fontSize: 16  }}>Sua foto de perfil</Text>
-        <TextInput
-          placeholder="Confirmar sua senha"
-          placeholderTextColor="#dbdbdb"
-          style={{
-            backgroundColor: colors.bgDark1,
-            padding: 15,
-            borderRadius: 10,
-            color: '#fff',
-            marginBottom: 20,
-          }}
-          secureTextEntry
-        />
-
-<Link href="/register3" asChild>
-  <TouchableOpacity
-    style={{
-      backgroundColor: colors.primary,
-      padding: 15,
-      borderRadius: 10,
-      alignItems: "center",
-    }}
-  >
-    <Text style={{ color: "#fff", fontWeight: "bold" }}>Registre</Text>
-  </TouchableOpacity>
-</Link>
-
-        <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 50 }}>
-          <Text style={{ color: '#fff', marginRight: 5 }}>Já possui conta?</Text>
-          <TouchableOpacity>
-         <Link href="/" asChild>
-  <TouchableOpacity>
-    <Text style={{ color: '#4399FF', fontWeight: 'bold' }}>Faça login</Text>
-  </TouchableOpacity>
-</Link>
-               
-            
-          </TouchableOpacity>
-          
+    <KeyboardAvoidingView style={{ flex: 1, backgroundColor: colors.bgDark4, paddingHorizontal: 20 }}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', paddingTop: 40 }}>
+          <Image source={PlaceholderImage} style={{ width: 150, height: 150 }} resizeMode="contain" />
+          <Text style={{ color: '#fff', fontWeight: 'bold', fontSize: 40, padding: 30 }}>Criar Conta</Text>
+          <Image source={GrowBar} style={{ marginBottom: 30 }} />
         </View>
+
+        <View style={{ flex: 1, justifyContent: 'flex-start', width: '100%' }}>
+          <Text style={{ color: '#fff', marginBottom: 5, fontWeight: 'medium', fontSize: 16 }}>Endereço</Text>
+          <TextInput
+            placeholder="Digite seu endereço"
+            placeholderTextColor="#dbdbdb"
+            style={{
+              backgroundColor: colors.bgDark1,
+              padding: 15,
+              borderRadius: 10,
+              color: '#fff',
+              marginBottom: 15,
+            }}
+          />
+
+          <Text style={{ color: '#fff', marginBottom: 5, fontWeight: 'medium', fontSize: 16 }}>Celular</Text>
+          <TextInput
+            placeholder="Digite seu celular"
+            placeholderTextColor="#dbdbdb"
+            style={{
+              backgroundColor: colors.bgDark1,
+              padding: 15,
+              borderRadius: 10,
+              color: '#fff',
+              marginBottom: 15,
+            }}
+          />
+
+          <Text style={{ color: '#fff', marginBottom: 5, fontWeight: 'medium', fontSize: 16 }}>Sua foto de perfil</Text>
+          
+       
+          <TouchableOpacity
+            onPress={pickImageAsync}
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              backgroundColor: colors.bgDark5,
+              padding: 12,
+              borderRadius: 10,
+              marginBottom: 20,
+            }}
+          >
+            <FontAwesome name="picture-o" size={28} color={colors.primary} />
+            <Text style={{ color: colors.primary, fontWeight: 'bold', marginLeft: 10 }}>
+              Acesse a galeria
+            </Text>
+          </TouchableOpacity>
+
+    
+          {selectedImage && (
+            <Image
+              source={{ uri: selectedImage }}
+              style={{ width: 50, height: 50, marginBottom: 20 }}
+            />
+          )}
+
         
-      </View>
-     </ScrollView>
+          <Link href="/register3" asChild>
+            <TouchableOpacity
+              style={{
+                backgroundColor: colors.primary,
+                padding: 15,
+                borderRadius: 10,
+                alignItems: 'center',
+                marginBottom: 20,
+              }}
+            >
+              <Text style={{ color: '#fff', fontWeight: 'bold' }}>Registre</Text>
+            </TouchableOpacity>
+          </Link>
+
+          {/* Link para login */}
+          <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 20 }}>
+            <Text style={{ color: '#fff', marginRight: 5 }}>Já possui conta?</Text>
+            <Link href="/" asChild>
+              <TouchableOpacity>
+                <Text style={{ color: '#4399FF', fontWeight: 'bold' }}>Faça login</Text>
+              </TouchableOpacity>
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 }
