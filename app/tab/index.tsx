17b99e01eb5282fc92b19b2d4c700f84cb5b5b1b
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-10-02 14:49:17 -0300

    feat: estruturando projeto

--- app/tab/index.tsx
+++ app/tab/index.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from 'react-native';
 
-export default function HomeScreen() {
+export default function LoginScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-zinc-100">
       <Text className="text-3xl font-extrabold text-purple-700">
