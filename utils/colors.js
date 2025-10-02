5e329390a40c26be7e2b2653e6b7fa9e08529437
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-10-02 15:26:23 -0300

    refactor: ajustando caminho das cores

---
+++ utils/colors.js
@@ -0,0 +1,20 @@
+// colors.js
+export const colors = {
+  bgDark1: '#2C2F33',
+  bgDark2: '#333E4B',
+  bgDark3: '#3D5675',
+  bgDark4: '#0E1114',
+  bgDark5: '#0A1B31',
+
+  primary: '#0673F6',
+  secondary: '#2670CA',
+  third: '#3867A0',
+
+  lightBg1: '#DADBDF',
+  lightBg2: '#79ACEA',
+
+  highlight1: '#05EFF5',
+  highlight2: '#F0C348',
+  highlight3: '#F68906',
+  highlight4: '#5E06F6',
+};
