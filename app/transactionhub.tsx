5777d9ffa016d7482bb33ddb21d58ea85b54d23f
leo-kina <f1boy.leokina@gmail.com>
2025-10-05 05:35:08 -0300

    feat:fim pagina transaction hub

--- app/transactionhub.tsx
+++ app/transactionhub.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Pressable, Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';  // Importe os ícones do Ionicons
 import FooterMenu from "../components/FooterMenu";
+import { colors } from 'utils/colors';
+
+const PlaceholderImage = require('../assets/logo/nexaspay_logo.png'); // Caminho para a imagem
 
 const TransactionHub = () => { 
   return (
     <View style={styles.screen}>
-      {/* Título da tela */}
-      <Text style={styles.headerTitle}>Nexas Pay</Text>
+      {/* Logo */}
+      <Image 
+        source={PlaceholderImage} 
+        style={styles.logoImage} // Ajuste o tamanho e o espaçamento da imagem, se necessário
+      />
 
       {/* Texto de instrução */}
       <Text style={styles.chooseText}>Escolha o tipo de transferência</Text>
@@ -14,15 +21,19 @@ const TransactionHub = () => {
       {/* Grid de Botões de escolha */}
       <View style={styles.buttonGrid}>
         <Pressable style={styles.chooseBtn}>
+          <Ionicons name="logo-usd" size={30} color="#0673F6" /> {/* Ícone para Pix */}
           <Text style={styles.chooseBtnText}>Pix</Text>
         </Pressable>
         <Pressable style={styles.chooseBtn}>
+          <Ionicons name="cash" size={30} color="#0673F6" /> {/* Ícone para TED */}
           <Text style={styles.chooseBtnText}>TED</Text>
         </Pressable>
         <Pressable style={styles.chooseBtn}>
+          <Ionicons name="logo-bitcoin" size={30} color="#0673F6" /> {/* Ícone para Crypto */}
           <Text style={styles.chooseBtnText}>Crypto</Text>
         </Pressable>
         <Pressable style={styles.chooseBtn}>
+          <Ionicons name="document-text" size={30} color="#0673F6" /> {/* Ícone para Boleto */}
           <Text style={styles.chooseBtnText}>Boleto</Text>
         </Pressable>
       </View>
@@ -38,29 +49,21 @@ const TransactionHub = () => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    backgroundColor: "#0B111A",
+    backgroundColor: colors.bgDark4,
     paddingTop: 40,
-    justifyContent: "space-between", // Garante que o footer fique fixo na parte inferior
-    alignItems: "center", // Centraliza o conteúdo horizontalmente
+    justifyContent: "space-between", 
+    alignItems: "center", 
   },
 
-  // Título da tela
-  headerTitle: {
-    color: "#fff",
-    fontSize: 24,
-    fontWeight: "700",
-    marginBottom: 20, // Distância entre o título e o texto
-  },
-
-  // Texto "Escolha o tipo de transferência"
+  
   chooseText: {
     color: "#fff",
-    fontSize: 16,
+    fontSize: 22,
     fontWeight: "600",
-    marginBottom: 20, // Espaço entre o texto e os botões
+    marginBottom: 10, 
   },
 
-  // Grid de Botões (2 colunas)
+  
   buttonGrid: {
     flexDirection: "row", 
     flexWrap: "wrap", 
@@ -68,30 +71,31 @@ const styles = StyleSheet.create({
     width: "100%",
   },
 
-  // Estilo para os botões de "Escolher"
   chooseBtn: {
-    backgroundColor: "#0A1B31", // Cor de fundo dos botões
-    paddingVertical: 14, // Tamanho ajustado do botão (mais alto)
-    paddingHorizontal: 25, // Mais largo
-    borderRadius: 20, // Bordas arredondadas
-    marginBottom: 15, // Espaço entre os botões
-    width: "45%", // Cada botão ocupa 45% da largura, assim fica 2 por linha
-    justifyContent: "center", // Alinhando o texto no centro
-    alignItems: "center", // Centralizando o texto do botão
+    flexDirection: "row",
+    backgroundColor: "#0A1B31", 
+    paddingVertical: 12, 
+    borderRadius: 20, 
+    marginBottom: 25,
+    width: "45%", 
+    justifyContent: "flex-start",
+    gap: 15,
+    paddingLeft: 15 
   },
 
-  // Cor e estilo do texto dentro dos botões
+
   chooseBtnText: {
     color: "#0673F6", 
     fontWeight: "700", 
-    fontSize: 16,
+    fontSize: 20,
+
   },
 
-  // Estilo para garantir que o Footer tenha altura adequada
+
   footerContainer: {
     width: "100%", 
-   
   },
+
 });
 
 export default TransactionHub;
